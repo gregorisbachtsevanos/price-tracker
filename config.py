c79d930a81eeb1bb9e61fc3a65cdc486ed5bfe79
Gregoris Bachtsevanos <94171068+gregorisbachtsevanos@users.noreply.github.com>
2025-08-04 15:27:23 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+ASSETS = {
+    "bitcoin": {"currency": "usd", "threshold": 60000},
+    "ethereum": {"currency": "usd", "threshold": 3500},
+    "solana": {"currency": "usd", "threshold": 100},
+}
+
+TO_EMAIL = "test@mail.com"
+ENABLE_EMAIL = True
+ENABLE_TELEGRAM = True
